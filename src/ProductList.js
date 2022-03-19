1fe25ec5bd6cd4ee7475691d7543695a9d8bd014
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-19 15:54:02 +0300

    create table

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Table } from 'reactstrap';
 
 export default class ProductList extends Component {
     constructor(props){
@@ -8,6 +9,21 @@ export default class ProductList extends Component {
         return (
             <div>
                 <h3>{this.props.info.title}-{this.props.currentCategory}</h3>
+                <Table>
+                    <thead>
+                        <th>Id</th>
+                        <th>CategoryId</th>
+                        <th>Product Name</th>
+                        <th>Quantity Per Unit</th>
+                        <th>Unit Price</th>
+                        <th>Units In Stock</th>
+                    </thead>
+                    <tbody>
+                        {
+                            
+                        }
+                    </tbody>
+                </Table>
             </div>
         )
     }
