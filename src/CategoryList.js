f74bf55d2866fed1c8017f7ae6922e641049de33
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-19 14:18:45 +0300

    state category list component

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        state:{};
+        this.state = {
+            counter: 4
+        };
     }
     render() {
         return (
             <div>
                 <h3>{this.props.info.title}</h3>
+                <h3>{this.state.counter}</h3>
                 <ListGroup>
                     <ListGroupItem>1</ListGroupItem>
                     <ListGroupItem>2</ListGroupItem>
