96b529864f870f28f4e76ec6dd11829704fe89e6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-19 13:42:34 +0300

    list group

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
     render() {
         return (
             <div>
                 <h3>Category List</h3>
+                <ListGroup>
+                    <ListGroupItem>1</ListGroupItem>
+                    <ListGroupItem>2</ListGroupItem>
+                    <ListGroupItem>3</ListGroupItem>
+                    <ListGroupItem>4</ListGroupItem>
+                    <ListGroupItem>5</ListGroupItem>
+                </ListGroup>
             </div>
         )
     }
