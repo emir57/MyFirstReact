a85f9b9fbacb474927183d5321e4e4406f42a0ce
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-19 17:22:09 +0300

    navbar

--- src/Navi.js
+++ src/Navi.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react'
+import { Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink, NavbarText, NavbarBrand, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap"
 
 export default class Navi extends Component {
     render() {
         return (
             <div>
-                <h2>Navi Component</h2>
+                <Navbar
+                    color="light"
+                    expand="md"
+                    fixed=""
+                    full
+                    light
+                >
+                    <NavbarBrand href="/">
+                        reactstrap
+                    </NavbarBrand>
+                    <NavbarToggler onClick={function noRefCheck() { }} />
+                    <Collapse navbar>
+                        <Nav
+                            className="me-auto"
+                            navbar
+                        >
+                            <NavItem>
+                                <NavLink href="/components/">
+                                    Components
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">
+                                    GitHub
+                                </NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown
+                                inNavbar
+                                nav
+                            >
+                                <DropdownToggle
+                                    caret
+                                    nav
+                                >
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Reset
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                        <NavbarText>
+                            Simple Text
+                        </NavbarText>
+                    </Collapse>
+                </Navbar>
             </div>
         )
     }
