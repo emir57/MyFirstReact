df2cb7110d625fcf8b51c8d0d01d68f92dc71c76
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-19 15:48:22 +0300

    get categories

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            categories: [
-                { categoryId: 1, categoryName: "Beverages" },
-                { categoryId: 2, categoryName: "Condiments" },
-                { categoryId: 3, categoryName: "Chocolates" },
-                { categoryId: 4, categoryName: "Foods" },
-            ],
-        };
+    state = {
+        categories: [],
+    };
+    getCategories = ()=>{
+        fetch("http://localhost:3000/categories")
+        .then(response=>response.json())
+        .then(data=>this.setState({categories:data}));
     }
     render() {
         return (
