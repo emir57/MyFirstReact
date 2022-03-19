342d8ecd108edd5526e9609c8f34c725a9dc1c27
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-19 14:00:48 +0300

    product list props

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 export default class ProductList extends Component {
+    constructor(props){
+        super(props);
+    }
     render() {
         return (
             <div>
-                <h3>Product List</h3>
+                <h3>{this.props.title}</h3>
             </div>
         )
     }
