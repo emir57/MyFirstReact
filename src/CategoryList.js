2d25ad804d76098039eeeb4e3156f7026466a021
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-19 14:52:29 +0300

    format

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -5,17 +5,17 @@ export default class CategoryList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            categories:[
-                {categoryId:1,categoryName:"Beverages"},
-                {categoryId:2,categoryName:"Condiments"},
-                {categoryId:3,categoryName:"Chocolates"},
-                {categoryId:4,categoryName:"Foods"},
+            categories: [
+                { categoryId: 1, categoryName: "Beverages" },
+                { categoryId: 2, categoryName: "Condiments" },
+                { categoryId: 3, categoryName: "Chocolates" },
+                { categoryId: 4, categoryName: "Foods" },
             ],
-            currentCategory:""
+            currentCategory: ""
         };
     }
-    changeCategory = (category)=>{
-        this.setState({currentCategory:category.categoryName});
+    changeCategory = (category) => {
+        this.setState({ currentCategory: category.categoryName });
     }
     render() {
         return (
@@ -23,8 +23,8 @@ export default class CategoryList extends Component {
                 <h3>{this.props.info.title}</h3>
                 <ListGroup>
                     {
-                        this.state.categories.map(category=>{
-                            return <ListGroupItem onClick={()=>this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+                        this.state.categories.map(category => {
+                            return <ListGroupItem onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
                         })
                     }
                 </ListGroup>
