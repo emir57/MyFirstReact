c7cc11a1bf63811f8164849a72b3e999fa628989
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-20 17:14:39 +0300

    refactoring cartSummary

--- src/CartSummary.js
+++ src/CartSummary.js
@@ -7,31 +7,36 @@ export default class CartSummary extends Component {
             return "Sepetinzde Ürün Yok"
         }
     }
+    renderSummary() {
+        return (<UncontrolledDropdown
+            inNavbar
+            nav
+        >
+            <DropdownToggle
+                caret
+                nav
+            >
+                Sepetim <Badge color='info'>{this.props.carts.length}</Badge>
+            </DropdownToggle>
+            <DropdownMenu right>
+                {
+                    this.props.carts.length > 0 ?
+                        this.props.carts.map(cart => {
+                            return <DropdownItem key={cart.product.id}>
+                                {cart.product.productName} <Badge color='primary'>{cart.quantity}</Badge>
+                            </DropdownItem>
+                        }) :
+                        this.getCarts()
+                }
+            </DropdownMenu>
+        </UncontrolledDropdown>
+        );}
     render() {
         return (
             <div>
-                <UncontrolledDropdown
-                    inNavbar
-                    nav
-                >
-                    <DropdownToggle
-                        caret
-                        nav
-                    >
-                        Sepetim <Badge color='info'>{this.props.carts.length}</Badge>
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                        {
-                            this.props.carts.length > 0 ?
-                                this.props.carts.map(cart => {
-                                    return <DropdownItem key={cart.product.id}>
-                                        {cart.product.productName} <Badge color='primary'>{cart.quantity}</Badge>
-                                    </DropdownItem>
-                                }) :
-                                this.getCarts()
-                        }
-                    </DropdownMenu>
-                </UncontrolledDropdown>
+                {
+                    this.props.carts.length>0?this.renderSummary():""
+                }
             </div>
         )
     }
