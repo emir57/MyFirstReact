9c61ba31ad9bf44126c9f60dfe21d589c5811dcd
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-21 17:57:13 +0300

    refactoring onChangeHandler

--- src/Form1Demo.js
+++ src/Form1Demo.js
@@ -3,10 +3,13 @@ import { Form, FormGroup,FormControl } from 'reactstrap'
 
 export default class Form1Demo extends Component {
     state = {
-        userName: ''
+        userName: '',
+        city:''
     }
     onChangeHandler = (event) => {
-        this.setState({ userName: event.target.value })
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]:value})
     }
     onSubmitHandler = (event) => {
         event.preventDefault();
@@ -17,8 +20,12 @@ export default class Form1Demo extends Component {
             <div>
                 <Form onSubmit={this.onSubmitHandler}>
                     <h3>User Name</h3>
-                    <input className='form-control' onChange={this.onChangeHandler} type="text"></input>
+                    <input name='userName' className='form-control' onChange={this.onChangeHandler} type="text"></input>
                     <h3>User Name is {this.state.userName}</h3>
+                    <hr></hr>
+                    <h3>City</h3>
+                    <input name='city' className='form-control' onChange={this.onChangeHandler} type="text"></input>
+                    <h3>City is {this.state.city}</h3>
                     <input type="submit" value="Save"></input>
                 </Form>
             </div>
