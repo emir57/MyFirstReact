6317101c5fbb73ac123cc479631bd38431fdb49a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-21 18:31:37 +0300

    handleChange

--- src/FormDemo2.js
+++ src/FormDemo2.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
-import { Button,Form,FormGroup,Label,Input } from 'reactstrap'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
 export default class FormDemo2 extends Component {
     state = {
-        email:'',
-        password:'',
-        city:'',
-        description:''
+        email: '',
+        password: '',
+        city: '',
+        description: ''
+    }
+    handleChange = event => {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]:value});
     }
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
