478751073963d831f3c4b87bebf66cdee59928b4
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-21 18:29:57 +0300

    add state

--- src/FormDemo2.js
+++ src/FormDemo2.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import { Button,Form,FormGroup,Label,Input } from 'reactstrap'
 
 export default class FormDemo2 extends Component {
+    state = {
+        email:'',
+        password:'',
+        city:'',
+        description:''
+    }
     render() {
         return (
             <div>
